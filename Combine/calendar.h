2a0e9c7837d6e6e2f7e46a30a41fe6871e7de8d8
salenkoanton <salenko3110@gmail.com>
2016-05-14 21:17:48 +0300

    calendar

--- Combine/calendar.h
+++ Combine/calendar.h
@@ -1,8 +1,23 @@
 #ifndef CALENDAR_H
 #define CALENDAR_H
+#include "month.h"
 class Calendar{
+private:
+    Month * month;
+public:
+    Calendar(int countDaysInMonth, int countDaysBef){
+        Day befDays[7];
+        for (int i = 0; i < 7; i++)
+            befDays[i] = Day ((25 + i) % 31 + 1); // prototype for this mounth
+        month = new Month(countDaysInMonth, countDaysBef, befDays);
 
+    }
+    Month * getMounth()
+    {
+        return month;
+    }
 };
+
 
 #endif // CALENDAR_H
 
