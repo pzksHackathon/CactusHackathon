792decbbdb7f54d9f15a16c72043dd27f00642ab
maximgonchar <maxgonchar9@gmail.com>
2016-05-14 16:10:29 +0300

    max gonchar commit

--- Front/OrganizemMe/organizerui.cpp
+++ Front/OrganizemMe/organizerui.cpp
@@ -1,3 +1,5 @@
+#include <QDesktopWidget>
+
 #include "organizerui.h"
 #include "ui_organizerui.h"
 
@@ -6,6 +8,14 @@ OrganizerUI::OrganizerUI(QWidget *parent) :
     ui(new Ui::OrganizerUI)
 {
     ui->setupUi(this);
+
+    // Disable resizing a BattleField window.
+    this->setWindowFlags(Qt::Window | Qt::MSWindowsFixedSizeDialogHint);
+    // Disable 'maximize' and 'hide' button in a BattleField window.
+    this->setWindowFlags(Qt::WindowMaximizeButtonHint | Qt::WindowCloseButtonHint);
+    // Move BattleField window to the center of screen.
+    this->move((QApplication::desktop()->width() - this->size().width()) / 2,
+               (QApplication::desktop()->height() - this->size().height()) / 2 - 50);
 }
 
 OrganizerUI::~OrganizerUI()
